07e3f276fa1f9da394f60b601e787530c47f03ed
Marcin Lewandowski <elem84@gmail.com>
2021-04-26 19:46:53 +0200

    Dodano producenta publikującego do centrali wiadomości

---
+++ producer_exchange.js
@@ -0,0 +1,31 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://app_full:app_full@localhost/app_1', function(error0, connection) {
+
+    if (error0)
+    {
+        throw error0;
+    }
+
+    connection.createChannel(function(error1, channel) {
+
+        if (error1)
+        {
+            throw error1;
+        }
+
+        var exchange = 'fanout_ex';
+        var msg = JSON.stringify({
+            'text': 'Hello world'
+        });
+
+        // tworzy centralę wiadomości
+        // channel.assertExchange(exchange, 'fanout', {
+        //     durable: false
+        // });
+
+        channel.publish(exchange, '', Buffer.from(msg));
+
+        console.log("[x] Sent %s", msg);
+    });
+});

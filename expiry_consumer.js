1f791d0f7a6d9c5fd8a9e51a66c94033ccb1e614
Marcin Lewandowski <elem84@gmail.com>
2021-04-27 19:58:11 +0200

    Dodano producenta i konsumenta  kolejki, która automatycznie zostanie usunięta po 15 sek. od rozłączenia konsumenta

---
+++ expiry_consumer.js
@@ -0,0 +1,26 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://app_full:app_full@localhost/app_1', function(err1, connection) {
+
+    if (err1)
+    {
+        return console.error(err1);
+    }
+
+    console.info('Connected');
+
+    connection.createChannel(function(err2, channel) {
+
+        if (err2)
+        {
+            throw err2;
+        }
+
+        channel.consume('q_expiry_app', function(msg) {
+
+            console.log(msg);
+        });
+
+        console.info('OK');
+    });
+});
---
+++ expiry_producer.js
@@ -0,0 +1,32 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://app_full:app_full@localhost/app_1', function(err1, connection) {
+
+    if (err1) {
+        return console.error(err1);
+    }
+
+    console.info('Connected');
+
+    connection.createChannel(function (err2, channel) {
+
+        if (err2) {
+            throw err2;
+        }
+
+        console.info('Channel created');
+
+        var queue = 'q_expiry_app';
+
+        channel.assertQueue(queue, {
+            expires: 15000
+        });
+
+        var msg = JSON.stringify({
+            'text': 'Hello world'
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+    });
+});

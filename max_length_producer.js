f6086c4e93d02e8f9d933c462f718d090a426aba
Marcin Lewandowski <elem84@gmail.com>
2021-04-27 20:05:20 +0200

    Dodano producenta tworzącego kolejkę z limitem wiadomości

---
+++ max_length_producer.js
@@ -0,0 +1,38 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://app_full:app_full@localhost/app_1', function(err1, connection) {
+
+    if (err1)
+    {
+        return console.error(err1);
+    }
+
+    console.info('Connected');
+
+    connection.createChannel(function(err2, channel) {
+
+        if (err2)
+        {
+            throw err2;
+        }
+
+        console.info('Channel created');
+
+        var queue = 'q_max_length_10_app';
+
+        channel.assertQueue(queue, {
+            maxLength: 10
+        });
+
+        for (var i = 0; i < 20; i++)
+        {
+            var msg = JSON.stringify({
+                'text': 'Message ' + i
+            });
+
+            console.log(" [x] Sent %s", msg);
+
+            channel.sendToQueue(queue, Buffer.from(msg));
+        }
+    });
+});

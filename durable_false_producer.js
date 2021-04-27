ae8f947980866c7eae8f60fb46d615f391159ee6
Marcin Lewandowski <elem84@gmail.com>
2021-04-27 19:59:00 +0200

    Dodano producenta ustawiającego parametr "durable" określający czy kolejka ma przeżyć restart serwera

---
+++ durable_false_producer.js
@@ -0,0 +1,32 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://app_full:app_full@localhost/app_1', function(err1, connection) {
+
+    if (err1) {
+        return console.error(err1);
+    }
+
+    console.info('Connected');
+
+    connection.createChannel(function (err2, channel) {
+
+        if (err2) {
+            throw err2;
+        }
+
+        console.info('Channel created');
+
+        var queue = 'q_durable_fale_app';
+        var msg = JSON.stringify({
+            'text': 'Czy wiadomość przetrwa restart serwera ??'
+        });
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log("OK");
+    });
+});

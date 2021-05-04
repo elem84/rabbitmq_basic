8de04862c3feb5c8d68423a624066fba06b5a2ab
Marcin Lewandowski <elem84@gmail.com>
2021-05-04 19:05:01 +0200

    Dodano obsługę klastra

---
+++ cluster/consumer.js
@@ -0,0 +1,28 @@
+let amqp = require('amqp-connection-manager');
+let q = 'tasks';
+
+let main = async () => {
+
+    var connection = amqp.connect([
+        'amqp://localhost:5672',
+        'amqp://localhost:5673',
+        'amqp://localhost:5674',
+    ]);
+
+    var channelWrapper = connection.createChannel({
+        json: true,
+        setup: function(channel) {
+            return channel.assertQueue(q, { durable: true });
+        }
+    });
+
+    channelWrapper.addSetup(function(channel) {
+        return Promise.all([
+            channel.consume(q, (msg) => {
+                console.log(msg.content.toString())
+            }, {noAck: true , exclusive: false })
+        ])
+    });
+}
+
+main()
